043a96ba74a7da405623c76a0172e7ad2b00d8a1
LAURA_MAYA DD <mayadray1@gmail.com>
2023-10-13 17:45:40 +0200

    exercice

--- Challenge15/src/app/user/user.component.ts
+++ Challenge15/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+
+  userForm = this.fb.group({
+    username: [''],
+    email: [''],
+    password: [''],
+    location: this.fb.group({
+      street: [''],
+      zipCode: [''],
+      city: ['']
+    })
+  });
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
+
+  constructor(private fb: FormBuilder) { }
+
 
 }
